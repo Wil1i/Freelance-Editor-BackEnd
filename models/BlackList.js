c6095c9f8bb717af88c6f1a83c08d317fd4c3cf7
Wil1i <williams.ca001@gmail.com>
2023-07-15 06:07:00 -0700

    Black list table configed

---
+++ models/BlackList.js
@@ -0,0 +1,21 @@
+const { DataTypes } = require("sequelize")
+
+const db = require("../configs/db")
+
+const BlackList = db.define("blacklists", {
+    id: {
+        type: DataTypes.INTEGER,
+        autoIncrement: true,
+        primaryKey: true
+    },
+
+    token: {
+        type: DataTypes.TEXT
+    },
+
+    userId: {
+        type: DataTypes.TEXT
+    }
+})
+
+module.exports = BlackList
